edb6ffed0b17459a48a0a16b5ce6ba8f92814899
Kate Somova <skagamine.15@gmail.com>
2025-10-27 08:30:45 +0300

    signup

--- test_auth.py
+++ test_auth.py
@@ -1,18 +1,52 @@
+import json
 from pprint import pprint
 
+import pytest
 import requests
-import json
 
-def test_post_token():
+
+@pytest.fixture()
+def get_token():
     body = json.dumps({
         "email": "tatarstan@gmail.com",
         "password": "string"
     })
     responce = requests.post(
         'https://mai-tech.ru/api/auth/login/',
-        data = body
+        data=body
+    ).json()
+    return responce['access_token']
+
+
+def test_get_token():
+    body = json.dumps({
+        "email": "tatarstan@gmail.com",
+        "password": "string"
+    })
+    responce = requests.post(
+        'https://mai-tech.ru/api/auth/login/',
+        data=body
     )
-    my_token = responce['access_token']
     assert responce.status_code == 200
     pprint(responce.json())
 
+
+def test_signup():
+    body = json.dumps({
+        "email": "testsssdd@example.com",
+        "password": "string",
+        "first_name": "Kates",
+        "last_name": "test",
+        "timezone": "string",
+        "organization": "string",
+        "phone_number": "+7(926)777-77-79",
+        "phone_country_code": "string",
+        "lang": "str",
+        "rate": "ele"
+    })
+    responce = requests.post(
+        'https://mai-tech.ru/api/auth/signup/',
+        data=body
+    )
+    assert responce.status_code == 200
+    pprint(responce.json())
--- test_pubs.py
+++ test_pubs.py
@@ -1,29 +1,33 @@
-from pprint import pprint
-from tabnanny import check
+import pytest_check as check
 import requests
 
+from test_auth import get_token
 
-def test_get_pub_list():
+
+# def test_get_pub_list():
+#    my_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjE1NzUzOTAsImlhdCI6MTc2MTUzOTM5MCwic2NvcGUiOiJhY2Nlc3NfdG9rZW4iLCJzdWIiOiJ0YXRhcnN0YW5AZ21haWwuY29tIn0.F9CpP-hMPulApA6GahylbtiaH6rXB8rn69royALh7Bk'
+#    headers = {
+#        'Authorization': f'Bearer {my_token}'
+#    }
+#    responce = requests.get(
+#        'https://mai-tech.ru/api/pubs/',
+#        headers=headers).json()
+#    print(responce)
+# Не отрабатывает, тайм-аут более 10 секунд
+
+
+def test_get_pub_detail(get_token):
     headers = {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjEzMzIzNjcsImlhdCI6MTc2MTI5NjM2Nywic2NvcGUiOiJhY2Nlc3NfdG9rZW4iLCJzdWIiOiJ0YXRhcnN0YW5AZ21haWwuY29tIn0.T8UneVmnMNqCmcSp4f4hfLz2AuWaWE8zRX9NoRDhZOs'
+        'Authorization': f"Bearer {get_token}"
     }
+    pub_id = 101
     responce = requests.get(
-        'https://mai-tech.ru/api/pubs/',
+        f'https://mai-tech.ru/api/pubs/{pub_id}',
         headers=headers).json()
-    print(responce)
-    # Не отрабатывает, тайм-аут более 10 секунд
-
-
-def test_get_pub_detail():
-    headers = {
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NjEzMzIzNjcsImlhdCI6MTc2MTI5NjM2Nywic2NvcGUiOiJhY2Nlc3NfdG9rZW4iLCJzdWIiOiJ0YXRhcnN0YW5AZ21haWwuY29tIn0.T8UneVmnMNqCmcSp4f4hfLz2AuWaWE8zRX9NoRDhZOs'
-    }
-    responce = requests.get(
-        'https://mai-tech.ru/api/pubs/101/',
-        headers=headers).json()
-    pprint(responce)
+    #  pprint(responce)
     # {'id': 101, 'theme_id': 3875813, 'title': None, 'text': None, 'highlights': None, 'is_main': False, 'trusted': True, 'url': None, 'source': None, 'force': None, 'lang': None, 'entities': {}, 'media': None, 'date': None, 'created_at': '2023-07-12T18:07:42.894142', 'updated_at': '2023-07-12T18:07:42.894142'}
-    with check:
-        assert responce['theme_id'] == 38758
+    with check.check():
+        assert responce['theme_id'] == 3875813
+    with check.check():
         assert responce['is_main'] is False
     print('okey')
